edc3d2574d40c1285da307bc2afa5701a7c2d0c7
GitHub Action <action@github.com>
2025-02-10 23:44:12 +0000

    Initialize template repository proper
    Set package name to com.github.puppy7777.uselessaddons
    Set project name to uselessaddons
    Set modid to uselessaddons

--- src/main/java/com/github/puppy7777/uselessaddons/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/puppy7777/uselessaddons/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.puppy7777.uselessaddons.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
